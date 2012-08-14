482fe8f5eb96b685198c76d05f27ac7444d12ae5
Miklos Gabor <wndmearle@yahoo.com>
2012-08-15 00:20:39 +0300

    Simple indented logger

---
+++ src/org/windom/util/IndentedLogger.java
@@ -0,0 +1,41 @@
+package org.windom.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndentedLogger {
+
+	private final Logger logger;
+	private final String indentUnit;
+	private String indentString;
+	
+	public IndentedLogger(Class<?> clazz, String indentUnit) {
+		this.logger = LoggerFactory.getLogger(clazz);
+		this.indentUnit = indentUnit;
+		this.indentString = "";
+	}
+	
+	public IndentedLogger(Class<?> clazz) {
+		this(clazz, "  ");
+	}
+	
+	public void indent() {
+		indentString += indentUnit;
+	}
+	
+	public void unindent() {
+		if (indentString.length() < indentUnit.length()) {
+			throw new RuntimeException("Unbalanced indent()-unindent()'s");
+		}
+		indentString = indentString.substring(indentUnit.length());
+	}
+	
+	public void info(String message, Object...args) {
+		logger.info(indentString + message, args);
+	}
+	
+	public void debug(String message, Object...args) {
+		logger.info(indentString + message, args);
+	}
+	
+}
