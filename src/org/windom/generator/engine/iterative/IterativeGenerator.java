3ea9cb3cc4337f51bec4ac9a27dd44442bf8a4ca
Miklos Gabor <wndmearle@yahoo.com>
2012-08-29 16:53:06 +0300

    Iterative generator - experimenting with basic expansion and backtracking

--- src/org/windom/generator/engine/iterative/IterativeGenerator.java
+++ src/org/windom/generator/engine/iterative/IterativeGenerator.java
@@ -1,10 +1,15 @@
 package org.windom.generator.engine.iterative;
 
+import java.util.List;
 import java.util.Random;
 
 import org.windom.generator.definition.Definition;
 import org.windom.generator.definition.Node;
+import org.windom.generator.definition.Rule;
+import org.windom.generator.definition.Symbol;
+import org.windom.generator.definition.Terminal;
 import org.windom.generator.engine.NodeInstance;
+import org.windom.generator.engine.RuleInstance;
 import org.windom.generator.engine.common.AbstractGenerator;
 import org.windom.generator.engine.common.GeneratorContext;
 
@@ -20,8 +25,105 @@ public class IterativeGenerator extends AbstractGenerator {
 	
 	@Override
 	protected NodeInstance generate(Node node, GeneratorContext ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		Waypoint initial = new Waypoint(null, null, 0, node);
+		generate(initial);
+		return initial.getNodeInstance();
+	}
+	
+	protected void generate(Waypoint current) {	
+		
+		Waypoint backtrack = null;
+		int which = 0;
+		
+		while (true) {
+			
+			NodeInstance nodeInstance = current.getNodeInstance();
+			Node node = nodeInstance.getNode();
+			
+			log.info("");
+			log.info("");
+			log.info("==============================");
+			log.info("waypoint: {}", current);
+			log.info("backtrack:");
+			for (Waypoint bt = backtrack; bt != null; bt = bt.getBacktrack()) {
+				log.info("{}", bt);
+			}
+			log.info("==============================");
+			
+			if (node instanceof Terminal) {
+				log.info("terminal {}", node);
+				which = current.getParentIdx();
+				current = current.getParent();
+				if (current == null) break;				
+				setPar(current, which, nodeInstance);
+				which++;
+				
+				if (((Terminal) node).getText().equals("c")) {
+					// backtrack
+					log.info("backtracking to {}", backtrack);
+					current = backtrack;
+					if (current == null) break;
+					backtrack = backtrack.getBacktrack();
+					current.setNodeInstance(new NodeInstance(current.getNodeInstance().getNode()));
+					continue;
+				}
+			} else if (node instanceof Symbol) {
+				RuleInstance ruleInstance = nodeInstance.getRuleInstance();
+				
+				if (ruleInstance == null) {
+					if (current.getApplicableRules().isEmpty()) {
+						// backtrack
+						log.info("backtracking to {}", backtrack);
+						current = backtrack;
+						if (current == null) break;
+						backtrack = backtrack.getBacktrack();
+						current.setNodeInstance(new NodeInstance(current.getNodeInstance().getNode()));
+						continue;
+					}
+					Rule rule = chooseAndRemoveRule(current.getApplicableRules());
+					ruleInstance = new RuleInstance(rule);
+					nodeInstance = new NodeInstance(nodeInstance.getNode(), ruleInstance);
+					current.setNodeInstance(nodeInstance);
+					which = 0;
+				}
+				
+				int todo = getTodo(ruleInstance, which);
+				log.info("todo {}", todo);
+				
+				if (todo < 0) {
+					which = current.getParentIdx();
+					current = current.getParent();
+					if (current == null) break;
+					setPar(current, which, nodeInstance);
+					which++;
+				} else {
+					Node nextNode = ruleInstance.getRule().getRight().get(todo);
+					current = new Waypoint(backtrack, current, todo, nextNode);
+					if (current.isDecisionPoint()) {
+						backtrack = current;
+					}
+				}
+			} else {
+				throw new RuntimeException("not yet ;)");
+			}
+		}
+	}
+	
+	private void setPar(Waypoint wp, int idx, NodeInstance nodeInstance) {
+		List<NodeInstance> is = wp.getNodeInstance().getRuleInstance().getNodeInstances();
+		if (idx > is.size()) {
+			throw new RuntimeException("na ez baj");
+		} else if (idx == is.size()) {
+			is.add(nodeInstance);
+		} else {
+			is.set(idx, nodeInstance);
+		}
+	}
+	
+	private int getTodo(RuleInstance ruleInstance, int which) {
+		if (which >= ruleInstance.getRule().getRight().size())
+			which = -1;
+		return which;
 	}
 	
 }
--- src/org/windom/generator/engine/iterative/Waypoint.java
+++ src/org/windom/generator/engine/iterative/Waypoint.java
@@ -9,24 +9,55 @@ import org.windom.generator.engine.NodeInstance;
 
 public class Waypoint {
 
-	private final Waypoint prev;
+	private final Waypoint backtrack;
 	private final Waypoint parent;
 	private final int parentIdx;
 	private NodeInstance nodeInstance;
 	private final List<Rule> applicableRules;
 	
-	public Waypoint(Waypoint prev, Waypoint parent, int parentIdx, Node node) {
-		this.prev = prev;
+	public Waypoint(Waypoint backtrack, Waypoint parent, int parentIdx, Node node) {
 		this.parent = parent;
 		this.parentIdx = parentIdx;
 		this.nodeInstance = new NodeInstance(node);
 		this.applicableRules = (node != null && node.symbol() != null)
 				? new ArrayList<Rule>(node.symbol().getRules())
 				: null;
+		this.backtrack = isDecisionPoint()
+				? backtrack
+				: null;
 	}
 
-	public Waypoint getPrev() {
-		return prev;
+	public boolean isDecisionPoint() {
+		return applicableRules != null && applicableRules.size() > 1;
+	}
+	
+	public String toString(boolean showLinks) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[ ");
+		sb.append(String.format("node: %s @%d; ", nodeInstance.getNode(), parentIdx));
+		sb.append(String.format("rule: %s; ", nodeInstance.getRuleInstance() != null
+				? nodeInstance.getRuleInstance().getRule()
+				: "null"));
+		sb.append(String.format("applicable: %s; ", applicableRules));
+		if (showLinks) {
+			sb.append(String.format("parent: %s; ", parent != null
+				? parent.toString(false)
+				: "null"));
+			sb.append(String.format("backtrack: %s; ", backtrack != null
+					? backtrack.toString(false)
+					: "null"));
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+	
+	@Override
+	public String toString() {
+		return toString(true);
+	}
+	
+	public Waypoint getBacktrack() {
+		return backtrack;
 	}
 	public Waypoint getParent() {
 		return parent;
